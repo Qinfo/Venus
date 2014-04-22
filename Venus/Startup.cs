8de7aea99609c072956a26b643d2e897449dc00e
galenzhao <guoquan1982@gmail.com>
2014-04-22 11:50:12 +0800

    add

---
+++ Venus/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Venus.Startup))]
+namespace Venus
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
